2a172b516b8be7b8df96678c62aed85a339939dd
Igor Leandro Dantas Medeiros <141191598+IGTag@users.noreply.github.com>
2024-08-03 12:11:19 -0300

    add: shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import { useState } from "react";
+import { Button } from "@/_components/ui/button";
 
 const Home = () => {
-  const [] = useState();
-
-  return <h1 className="text-red-500 mb-50">Text Example</h1>;
+  return <Button>test</Button>;
 };
 
 export default Home;
